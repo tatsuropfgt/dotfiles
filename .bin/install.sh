d136edb5796f694838e28e520a7bbf362d4baa89
InabaTatsuro <tatsuro.pianooo@gmail.com>
2023-03-09 22:43:52 +0900

    [fix] install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -47,5 +47,5 @@ while [ $# -gt 0 ];do
 done
 
 link_to_homedir
-git config --global include.path "~/.gitconfig_shared"
+git config --global include.path "$HOME/.gitconfig_shared"
 command echo -e "\e[1;36m Install completed!!!! \e[m"
