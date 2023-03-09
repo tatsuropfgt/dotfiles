5ce1d28461789d275a43eef5cead2334cb92f6e9
InabaTatsuro <tatsuro.pianooo@gmail.com>
2023-03-09 23:21:04 +0900

    [fix] install.sh

--- .bin/install.sh
+++ .bin/install.sh
@@ -16,13 +16,13 @@ link_to_homedir() {
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
   local dotdir=$(dirname ${script_dir})
   if [[ "$HOME" != "$dotdir" ]];then
-    for f in $dotdir/.??*; do
-      [[ $(basename $f)$ == ".git" ]] && continue
-      if [[ -L "$HOME/$(basename $f)$" ]];then
-        command rm -f "$HOME/$(basename $f)$"
+    for f in "$dotdir"/.??*; do
+      [[ $(basename $f) == ".git" ]] && continue
+      if [[ -L "$HOME/$(basename $f)" ]];then
+        command rm -f "$HOME/$(basename $f)"
       fi
-      if [[ -e "$HOME/$(basename $f)$" ]];then
-        command mv "$HOME/$(basename $f)$" "$HOME/.dotbackup"
+      if [[ -e "$HOME/$(basename $f)" ]];then
+        command mv "$HOME/$(basename $f)" "$HOME/.dotbackup"
       fi
       command ln -snf $f $HOME
     done
