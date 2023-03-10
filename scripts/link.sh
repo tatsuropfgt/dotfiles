de56f1dca8a712d7ef3adebbd159dac1b72d90ec
InabaTatsuro <tatsuro.pianooo@gmail.com>
2023-03-11 03:30:06 +0900

    [fix] link.sh

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-dotfiles_root=$(cd $(dirname $0)/.. && pwd)
+dotfiles_root=$(cd "$(dirname $0)"/.. && pwd)
 
 # dotfilesディレクトリの中身のリンクをホームディレクトリ直下に作成
-cd ${dotfiles_root}/dotfiles
+cd ${dotfiles_root}/dotfiles || exit' or 'cd ${dotfiles_root}/dotfiles || return
 for file in .*; do
     ln -s ${PWD}/${file} ${HOME}
 done
